f149ab319b5b19563d99421dc2f50344237d0049
Luidgi <clairboy@ext.lemonde.fr>
2022-09-19 10:00:52 +0200

    feat(zone-title): add close button for each information tooltip

--- src/components/UsageSection/UsageSection.jsx
+++ src/components/UsageSection/UsageSection.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { ChevronLeftIcon } from '@heroicons/react/solid';
 import { Tooltip } from 'flowbite-react';
@@ -18,6 +18,7 @@ export default function UsageSection () {
   const selectedFormats = useSelector(state => state.istexApi.selectedFormats);
   const usage = useSelector(state => state.istexApi.usage);
   const [shouldDisplayUsage, setShouldDisplayUsage] = useState(true);
+  const [showTooltipContent, setShowTooltipContent] = useState(true);
 
   const handleDisplayingOfUsage = usageName => setShouldDisplayUsage(usageName !== 'customUsage');
 
@@ -81,24 +82,39 @@ export default function UsageSection () {
     eventEmitter.addListener(events.setUsage, usageHandler);
   }, []);
 
+  const nodeRef = useRef([]);
+  const onCloseClick = (index) => {
+    nodeRef.current[index].click();
+  };
   return (
     <div className='my-12'>
       <TitleSection
         title='Usage'
         num='2'
         infoTextTitle=''
+        showTooltipContent={showTooltipContent}
         infoTextContent={
-          <p className='text-sm text-white'>
-            Le choix du mode "Usage<br />
-            personnalisé" donne accès à tous les<br />
-            types de fichiers et de formats<br />
-            existants dans ISTEX. En revanche, le<br />
-            choix d’une plateforme ou d’un outil<br />
-            particuliers induit une sélection<br />
-            automatique des formats et types de<br />
-            fichiers qui seront extraits.<br />
-            Voir la <a className='font-bold text-istcolor-blue cursor-pointer' href='https://doc.istex.fr/tdm/extraction/istex-dl.html#usage_1'>documentation ISTEX </a>.
-          </p>
+          <>
+            <div className='flex w-full justify-end relative left-1'>
+              <button type='button' onClick={() => setShowTooltipContent(!showTooltipContent)} className='w-4 h-4 bg-white rounded-full  inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500'>
+                <span className='sr-only'>Fermer l'info bulle</span>
+                <svg className='h-6 w-6' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor' aria-hidden='true'>
+                  <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M6 18L18 6M6 6l12 12' />
+                </svg>
+              </button>
+            </div>
+            <p className='text-sm text-white'>
+              Le choix du mode "Usage<br />
+              personnalisé" donne accès à tous les<br />
+              types de fichiers et de formats<br />
+              existants dans ISTEX. En revanche, le<br />
+              choix d’une plateforme ou d’un outil<br />
+              particuliers induit une sélection<br />
+              automatique des formats et types de<br />
+              fichiers qui seront extraits.<br />
+              Voir la <a className='font-bold text-istcolor-blue cursor-pointer' href='https://doc.istex.fr/tdm/extraction/istex-dl.html#usage_1'>documentation ISTEX </a>.
+            </p>
+          </>
         }
       />
       <p className='text-sm md:text-base'>Cliquez sur l’usage visé pour votre corpus :</p>
@@ -131,11 +147,11 @@ export default function UsageSection () {
             are both the custom usage */}
             {(usageName === 'customUsage' && usage === 'customUsage') && !shouldDisplayUsage && (
               <div className='flex flex-col md:flex-row gap-x-8 gap-y-4'>
-                {Object.keys(formats).map(formatCategory => {
+                {Object.keys(formats).map((formatCategory, index) => {
                   // Cases of covers and annexes which are not in a category
                   if (formats[formatCategory].value !== undefined) {
                     return (
-                      <div key={formatCategory} className='font-semibold capitalize mx-5 md:mx-0'>
+                      <div key={index} className='font-semibold capitalize mx-5 md:mx-0'>
                         <Format
                           isSubCategory={false}
                           className='font-bold capitalize'
@@ -169,9 +185,21 @@ export default function UsageSection () {
                           {formats[formatCategory].label}
                           <Tooltip
                             trigger='click'
-                            content={formatInfoText[formatCategory].infoText}
+                            content={
+                              <>
+                                <div className='flex w-full justify-end relative left-1'>
+                                  <button type='button' onClick={() => onCloseClick(index)} className='w-4 h-4 bg-white rounded-full  inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500'>
+                                    <span className='sr-only'>Fermer l'info bulle</span>
+                                    <svg className='h-6 w-6' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor' aria-hidden='true'>
+                                      <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M6 18L18 6M6 6l12 12' />
+                                    </svg>
+                                  </button>
+                                </div>
+                                {formatInfoText[formatCategory].infoText}
+                              </>
+                            }
                           >
-                            <button>
+                            <button ref={(ref) => { nodeRef.current[index] = ref; }}>
                               <FontAwesomeIcon icon='circle-info' className='text-istcolor-blue pl-2 cursor-pointer' />
                             </button>
                           </Tooltip>
